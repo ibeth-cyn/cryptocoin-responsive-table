4d494a5e90edfbdd0ef6b87adec2f0ee762231fd
Elizabeth Akpan <eakpan15@alustudent.com>
2019-12-08 02:35:34 +0400

    Display coins on HTML  page

--- script.js
+++ script.js
@@ -12,14 +12,41 @@
  /*Limit to 10 coins per page*/
 
  let apiKey =  "https://api.coinlore.com/api/tickers/?start=10&limit=10";
+ let coinsArray;
  
  $.getJSON(
     apiKey, 
      function(data){
+         coinsArray = data.data;
          console.log(data);
          appendDataToBody(data);
      });
 
 function appendDataToBody(data){
-    
+    const tBody = document.getElementsByTagName("tbody")[0]; 
+    for(let i = 0; i < coinsArray.length; i++){
+        const tRead = document.createElement("tr");
+        
+        tBody.append(tRead);
+
+        const th1 = document.createElement("th");
+        const th2 = document.createElement("th");
+        const th3 = document.createElement("th");
+        const th4 = document.createElement("th");
+
+        th1.innerText = coinsArray[i].name;
+        th2.innerText = coinsArray[i].symbol;
+        th3.innerText = "$ " + coinsArray[i].price_usd;
+        th4.innerText = coinsArray[i].tsupply + " " + coinsArray[i].symbol; 
+
+        console.log(th1);
+        console.log(th2);
+        console.log(th3);
+        console.log(th4);
+
+        tBody.appendChild(th1);
+        tBody.appendChild(th2);
+        tBody.appendChild(th3);
+        tBody.appendChild(th4);
+    }
 }
