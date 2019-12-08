2f797defecd88f139cf651bd2418e7fc0097dd2c
Elizabeth Akpan <eakpan15@alustudent.com>
2019-12-08 11:41:43 +0400

    Clean up javascript and in preparation for styling

--- script.js
+++ script.js
@@ -5,10 +5,9 @@
  *    Get 10 coins per "page"
  */
 
- /*Get API and connect to it - https://api.coinlore.com/api/tickers/?start=100&limit=100 * ✅/
- /*So, we are looking at 10 pages*/
- /*Create tr and add to tbody*/
- /*Create th and add to tr*/
+ /*Get API and connect to it - https://api.coinlore.com/api/tickers/?start=100&limit=100 ✅* /
+ /*Create tr and add to tbody✅* /
+ /*Create td and add to tr ✅*/
  /*Limit to 10 coins per page*/
 
  let apiKey =  "https://api.coinlore.com/api/tickers/?start=10&limit=10";
@@ -18,35 +17,29 @@
     apiKey, 
      function(data){
          coinsArray = data.data;
-         console.log(data);
          appendDataToBody(data);
      });
 
 function appendDataToBody(data){
     const tBody = document.getElementsByTagName("tbody")[0]; 
     for(let i = 0; i < coinsArray.length; i++){
-        const tRead = document.createElement("tr");
-        
-        tBody.append(tRead);
+        const tRows = document.createElement("tr");
 
-        const th1 = document.createElement("th");
-        const th2 = document.createElement("th");
-        const th3 = document.createElement("th");
-        const th4 = document.createElement("th");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+        const td4 = document.createElement("td");
 
-        th1.innerText = coinsArray[i].name;
-        th2.innerText = coinsArray[i].symbol;
-        th3.innerText = "$ " + coinsArray[i].price_usd;
-        th4.innerText = coinsArray[i].tsupply + " " + coinsArray[i].symbol; 
+        td1.innerText = coinsArray[i].name;
+        td2.innerText = coinsArray[i].symbol;
+        td3.innerText = "$ " + coinsArray[i].price_usd;
+        td4.innerText = coinsArray[i].tsupply + " " + coinsArray[i].symbol; 
 
-        console.log(th1);
-        console.log(th2);
-        console.log(th3);
-        console.log(th4);
+        tRows.appendChild(td1);
+        tRows.appendChild(td2);
+        tRows.appendChild(td3);
+        tRows.appendChild(td4);
 
-        tBody.appendChild(th1);
-        tBody.appendChild(th2);
-        tBody.appendChild(th3);
-        tBody.appendChild(th4);
+        tBody.append(tRows);
     }
 }
